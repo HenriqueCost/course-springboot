f612a787af07cd49a3d8db1939500d6bfbe231dc
HenriqueCost <henrique.costa@ibm.com>
2022-08-12 12:14:40 -0300

    Exception handling - delete

--- src/main/java/com/henriquecosta/course/servicies/UserService.java
+++ src/main/java/com/henriquecosta/course/servicies/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.henriquecosta.course.entities.User;
 import com.henriquecosta.course.repositories.UserRepository;
+import com.henriquecosta.course.servicies.exceptions.DatabaseException;
 import com.henriquecosta.course.servicies.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
